86d1caf0469baaf183963f7acc2bfd597170498d
Ekaitz Garcia <clase.ekaitz@gmail.com>
2026-03-25 12:49:15 +0100

    Guardado 1

---
+++ src/modelo/Interface_Vista_Producto.java
@@ -0,0 +1,8 @@
+package modelo;
+
+import java.util.Map;
+
+public interface Interface_Vista_Producto {
+
+	public void verProductos(String ref_producto, Map<Integer, Producto> productos) throws Exception;
+	}
