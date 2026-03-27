c4190c3a215012ae407af08ea985c35e1345b50b
EkaitzGarcia <clase.ekaitz@gmail.com>
2026-03-27 13:02:20 +0100

    Bebe agua

--- Okapi/src/vistas/Alta_Producto.java
+++ Okapi/src/vistas/Alta_Producto.java
@@ -10,18 +10,19 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 public class Alta_Producto extends JDialog implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
-	
-	private static Vista_Producto prod; //Vista
-	
-	private JButton btnAlta, btnCancelar; //Botones
-	
+
+	private static Vista_Producto prod; // Vista
+
+	private JButton btnAlta, btnCancelar; // Botones
 
 	/**
 	 * Launch the application.
@@ -46,25 +47,61 @@ public class Alta_Producto extends JDialog implements ActionListener {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
-		
+
 		btnAlta = new JButton("Add");
 		btnAlta.setBounds(341, 232, 85, 21);
 		btnAlta.addActionListener(this);
 		contentPanel.add(btnAlta);
-		
+
 		btnCancelar = new JButton("Cancel");
 		btnCancelar.setBounds(10, 232, 85, 21);
 		btnCancelar.addActionListener(this);
 		contentPanel.add(btnCancelar);
+
+		JLabel lblNewLabel = new JLabel("Reference:");
+		lblNewLabel.setBounds(36, 36, 59, 13);
+		contentPanel.add(lblNewLabel);
+
+		refText = new JTextField();
+		refText.setBounds(36, 52, 96, 19);
+		contentPanel.add(refText);
+		refText.setColumns(10);
+
+		JLabel lblNewLabel_1 = new JLabel("Name:");
+		lblNewLabel_1.setBounds(261, 36, 59, 13);
+		contentPanel.add(lblNewLabel_1);
+
+		nomText = new JTextField();
+		nomText.setBounds(261, 52, 96, 19);
+		contentPanel.add(nomText);
+		nomText.setColumns(10);
+
+		JLabel lblNewLabel_2 = new JLabel("Price:");
+		lblNewLabel_2.setBounds(36, 120, 59, 13);
+		contentPanel.add(lblNewLabel_2);
+
+		preText = new JTextField();
+		preText.setBounds(36, 134, 96, 19);
+		contentPanel.add(preText);
+		preText.setColumns(10);
+
+		JLabel lblNewLabel_3 = new JLabel("Discount:");
+		lblNewLabel_3.setBounds(261, 120, 59, 13);
+		contentPanel.add(lblNewLabel_3);
+
+		descText = new JTextField();
+		descText.setBounds(261, 134, 96, 19);
+		contentPanel.add(descText);
+		descText.setColumns(10);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getSource() == btnAlta) {
-			
+		if (e.getSource() == btnAlta) {
+
 		}
-		if(e.getSource() == btnCancelar) {
+		if (e.getSource() == btnCancelar) {
 			dispose();
 		}
 	}
