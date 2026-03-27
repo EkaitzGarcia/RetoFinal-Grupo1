3d1dece6f757bba27d45d0367460a594a3b6e1c1
EkaitzGarcia <clase.ekaitz@gmail.com>
2026-03-27 09:28:17 +0100

    Eliminar SRC

--- src/modelo/Interface_Vista_Producto.java
+++
@@ -1,8 +0,0 @@
-package modelo;
-
-import java.util.Map;
-
-public interface Interface_Vista_Producto {
-
-	public void verProductos(String ref_producto, Map<Integer, Producto> productos) throws Exception;
-	}
